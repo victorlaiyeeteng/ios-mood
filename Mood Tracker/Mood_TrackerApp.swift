10ffc1bf8735989c0bbd1482e070f64f7d659b92
victorlaiyeeteng <vlai69863@gmail.com>
2024-12-06 16:57:21 +0800

    Initial Commit

---
+++ Mood Tracker/Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_TrackerApp.swift
+//  Mood Tracker
+//
+//  Created by Victor Lai on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
